481fb69360161018f8e5e7890134574296d03f12
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2018-03-28 23:38:56 +0200

    updated backup script

--- backup_jupyterhub.sh
+++ backup_jupyterhub.sh
@@ -1,1 +1,7 @@
 docker run --rm   -u root   -v /tmp:/backups   -v jupyterhub-user-kbobrowski:/notebooks   jupyter/scipy-notebook   tar cvf /backups/kbobrowski-backup.tar /notebooks
+cp /tmp/kbobrowski-backup.tar /home/kamil/projects/
+cd /home/kamil/projects
+tar -xvf kbobrowski-backup.tar
+mv notebooks volume-jupyterhub
+cd volume-jupyterhub/machine-learning
+git remote set-url origin git@github.com:kbobrowski/machine-learning
